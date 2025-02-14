904fbee4356b922dfaeceae93e0509f86902520f
ShritejC <160143637+ShritejC@users.noreply.github.com>
2025-02-14 11:38:23 +0530

    Add files via upload

---
+++ Assesment_complete_2.sql
@@ -0,0 +1,140 @@
+use AdventureWorks2022
+
+--A. Find employee having highest rate or highest pay frequency
+
+select * from HumanResources.EmployeePayHistory
+select * from Person.Person
+
+select top 1 max(eph.Rate)Rate,concat_ws(' ',p.FirstName,p.LastName)Name
+from HumanResources.EmployeePayHistory eph
+join Person.Person p
+on p.BusinessEntityID=eph.BusinessEntityID
+group by eph.BusinessEntityID,p.FirstName,p.LastName
+order by rate desc 
+
+-- B. Analyze the inventory based on the shelf wise count of the product and their quantity
+
+select * from Production.Product
+select * from Production.ProductInventory
+
+select sum(pi.Quantity)qty,pi.shelf,count(pi.ProductID)cnt
+from Production.Product p
+join Production.ProductInventory pi
+on p.ProductID=pi.ProductID
+group by pi.shelf
+order by pi.shelf 
+
+-- C. Find the personal details with address and address type
+
+select * from Person.Person       --beid, firstname
+select * from Person.Address      --addid, address
+select * from Person.AddressType  --atid,  Name
+select* from Person.BusinessEntityAddress  --beid,addid,atid
+
+select p.FirstName,a.AddressLine1,at.Name
+from Person.BusinessEntityAddress beid
+join  Person.AddressType at
+on beid.AddressTypeID=at.AddressTypeID
+join Person.Address  a
+on a.AddressID=beid.AddressID
+join Person.Person p
+ON p.BusinessEntityID=beid.BusinessEntityID
+
+-- D. Find the job title having more revised payments
+
+select * from HumanResources.EmployeePayHistory
+select * from HumanResources.Employee
+
+select top 1 e.JobTitle, count(eph.RateChangeDate)rate_change_cnt
+from HumanResources.EmployeePayHistory eph
+join HumanResources.Employee e
+on eph.BusinessEntityID=e.BusinessEntityID
+group by e.JobTitle
+order by rate_change_cnt desc
+
+-- E. Display special offer description, category and avg (discount pct) per the month
+
+select * from Sales.SpecialOffer
+
+select Category,Description,avg(DiscountPct)disc,month(StartDate)months
+from Sales.SpecialOffer 
+group by Category,Description,StartDate
+
+-- G. Using rank and dense rank find territory wise top sales person
+
+select CONCAT_WS(' ',p.FirstName,p.LastName)as name,sp.TerritoryID,rank()over (order by st.TerritoryID),
+DENSE_RANK()over(order by st.TerritoryID) from person.Person p,Sales.SalesTerritory st,Sales.SalesPerson sp
+where p.BusinessEntityID=sp.BusinessEntityID and st.TerritoryID=sp.TerritoryID
+
+-- H. Calculate total years of experience of the employee and 
+--    find out employees those who server for more than 20 years
+
+select * from HumanResources.Employee
+
+select BusinessEntityID,HireDate from HumanResources.Employee 
+where year(GETDATE())-year(HireDate)>20                        --NULL
+
+-- I. Find the employee who is having more vacations than 
+--    the average vacation taken by all employees
+
+select * from HumanResources.Employee
+select * from Person.Person
+
+select e.VacationHours,
+(select concat_ws(' ',FirstName,LastName) from Person.Person p where p.BusinessEntityID=e.BusinessEntityID)Name,
+(select p.BusinessEntityID from Person.Person p where p.BusinessEntityID=e.BusinessEntityID)Beid
+from HumanResources.Employee e
+where e.VacationHours>(select avg(e.VacationHours) from HumanResources.Employee e)
+
+-- k. Find the department name having more employees
+
+select * from HumanResources.Department  --dptid,  Name
+select * from HumanResources.Employee    --beid
+select * from HumanResources.EmployeeDepartmentHistory  --beid,dptid
+
+select top 1 d.Name,count(e.BusinessEntityID)emp_cnt
+from HumanResources.EmployeeDepartmentHistory edh
+join HumanResources.Employee e
+on e.BusinessEntityID=edh.BusinessEntityID
+join HumanResources.Department d
+on d.DepartmentID=edh.DepartmentID
+group by d.Name
+order by emp_cnt desc
+
+-- L. Is there any person having more than one credit card (hint: PersonCreditCard)
+
+select * from sales.PersonCreditCard
+select * from Person.Person
+
+select  count(CreditCardID)cnt,pcc.BusinessEntityID,p.FirstName
+from Sales.PersonCreditCard pcc,Person.Person p
+where pcc.BusinessEntityID=p.BusinessEntityID
+group by pcc.BusinessEntityID,p.FirstName
+having count(CreditCardID)>1
+
+-- M. Find how many subcategories are available per category. (product sub category)
+
+select * from Production.ProductSubcategory  
+
+select  ProductCategoryID,count(ProductSubcategoryID)subcatID from Production.ProductSubcategory
+group by ProductcategoryID
+
+-- N. Find total standard cost for the active Product 
+--    where end date is not updated. (Productcost history)
+
+select * from Production.ProductCostHistory
+
+select ProductID,StandardCost from Production.ProductCostHistory
+where EndDate IS NULL
+
+-- O. Which territory is having more customers (hint: customer)
+
+select * from Sales.Customer
+select * from sales.SalesTerritory
+
+select top 1 st.Name,st.TerritoryID,count(c.CustomerID)cnt
+from Sales.Customer c
+join sales.SalesTerritory st 
+on st.TerritoryID=c.TerritoryID
+group by st.Name,st.TerritoryID
+order by cnt desc 
---
+++ Assessment_complete_1.sql
@@ -0,0 +1,166 @@
+use AdventureWorks2022                    -- e,c,h
+
+-- A.  Find first 20 employees who joined very early in the company
+
+select * from HumanResources.Employee
+select * from Person.Person
+
+select concat_ws(' ',FirstName,LastName)Emp_Name from Person.Person p where p.BusinessEntityID in
+(select top 20 BusinessEntityID from HumanResources.Employee e order by e.HireDate asc)
+
+-- B.  Find all employees name , job title, card details whose credit card 
+--     expired in the month 9 and year as 2009
+
+select * from Person.Person  --beid,Name      p
+select * from Sales.CreditCard   -- cID,expiry    cc
+select * from Sales.PersonCreditCard  --beid,cID      pcc
+select * from HumanResources.Employee  -- beid,jobtitle   e
+
+select p.FirstName,e.JobTitle,pcc.CreditCardID
+from Sales.PersonCreditCard pcc
+join Sales.CreditCard cc
+on pcc.CreditCardID=cc.CreditCardID
+join Person.Person p
+on p.BusinessEntityID=pcc.BusinessEntityID
+join HumanResources.Employee e
+on e.BusinessEntityID=pcc.BusinessEntityID
+where cc.ExpMonth=9 and cc.ExpYear=2009
+
+--C. Find the store address and contact number based on tables store 
+--   and Business entity check if any other table is required.
+
+select * from Sales.Store  --bussiness entity id,nameofstore,salespersonid
+select * from sales.vStoreWithAddresses --bussinessentiity id,addressline 1
+select * from Sales.vStoreWithContacts --bussiensseentiid,phonenumber
+
+select s.Name,sa.AddressLine1,sc.PhoneNumber from sales.store s,sales.vStoreWithAddresses sa,Sales.vStoreWithContacts sc where
+s.BusinessEntityID=sa.BusinessEntityID and sa.BusinessEntityID=sc.BusinessEntityID
+
+-- D.  check if any employee from job candidate table is having any payment revisions
+
+select * from HumanResources.JobCandidate   --jobcid,beid
+select * from HumanResources.EmployeePayHistory  --beid,ratechange date
+select * from HumanResources.Employee  --Beid
+
+select jc.BusinessEntityID from HumanResources.JobCandidate jc
+join HumanResources.EmployeePayHistory eph on eph.BusinessEntityID=jc.BusinessEntityID
+join HumanResources.Employee e on e.BusinessEntityID=e.BusinessEntityID
+group by jc.BusinessEntityID having count(eph.RateChangeDate)>1
+
+-- E. check colour wise standard cost
+
+select * from Production.Product
+
+select Color,avg(StandardCost)std_cost 
+from Production.Product 
+where Color is not null
+group by Color
+
+-- F. Which product is purchased more? (purchase order details)
+
+select * from Purchasing.PurchaseOrderDetail  --pid,orderqty
+select * from Production.Product   -- pid,Name
+ 
+select top 1 max(pod.OrderQty)cnt,p.Name
+from Purchasing.PurchaseOrderDetail pod,Production.Product p
+where p.ProductID=pod.ProductID
+group by p.Name
+order by cnt desc
+
+-- G.  Find the total values for line total product having maximum order
+
+select * from Production.Product  -- pid ,name
+select * from Sales.SalesOrderDetail  --pid
+
+select top 1 p.Name,sum(sod.LineTotal)total,sum(sod.OrderQty)qty
+from Production.Product p
+join Sales.SalesOrderDetail sod
+on sod.ProductID=p.ProductID
+group by sod.ProductID,p.Name
+order by qty desc
+
+-- H.  Which product is the oldest product as on the date (refer  the product sell start date)
+
+select * from Production.Product  -- pid,name,sell startdate
+
+select top 1 year(GETDATE())-year(SellStartDate)prod_age,Name,SellStartDate 
+from Production.Product
+
+ -- I. Find all the employees whose salary is more than the average salary
+
+select * from HumanResources.EmployeePayHistory  --beid,rate
+select * from Person.Person  -- beid,Name
+
+select eph.Rate,
+(select concat_ws(' ',FirstName,LastName) from Person.Person p where p.BusinessEntityID=eph.BusinessEntityID)Name,
+(select p.BusinessEntityID from Person.Person p where p.BusinessEntityID=eph.BusinessEntityID)Beid
+from HumanResources.EmployeePayHistory eph
+where eph.Rate>(select avg(Rate) from HumanResources.EmployeePayHistory)
+
+-- J. Display country region code, group average sales quota based on territory id 
+
+select * from Sales.SalesPersonQuotaHistory  --beid,sales quota
+select * from Sales.SalesTerritory  -- tid,crcode,group
+select * from Sales.SalesTerritoryHistory --beid,tid
+
+select avg(qh.SalesQuota)avg_sales_Quota,sth.TerritoryID,st.CountryRegionCode
+from Sales.SalesTerritoryHistory sth
+join Sales.SalesTerritory st
+on st.TerritoryID=sth.TerritoryID
+join Sales.SalesPersonQuotaHistory qh
+on qh.BusinessEntityID=sth.BusinessEntityID
+group by sth.TerritoryID,st.CountryRegionCode
+
+--k. Find the average age of male and female
+
+select*from HumanResources.Employee
+
+select avg((year(GETDATE())-year(BirthDate)))Avg_age,Gender
+from HumanResources.Employee
+group by Gender
+
+-- L. Which territory having more stores
+
+select * from Sales.Customer
+select * from sales.SalesTerritory
+
+select top 1 count(StoreID)store_cnt,st.Name
+from Sales.Customer c
+join sales.SalesTerritory st
+on st.TerritoryID = c.TerritoryID
+group by st.Name
+order by store_cnt desc
+
+-- M. Check for sales person details  which are working in Stores (find the sales person ID)
+
+select * from sales.Store  -- beid
+select * from Sales.SalesPerson --beid,tid
+select * from sales.SalesTerritoryHistory  -- beid,tid
+
+select  sp.BusinessEntityID
+from sales.Store s 
+join Sales.SalesPerson sp
+on sp.BusinessEntityID=s.BusinessEntityID
+
+-- N.  display the product name and product price 
+--     and count of product cost revised (productcost history)
+
+select * from Production.ProductCostHistory  --pid
+select * from Production.Product   -- pid
+
+select p.Name,count(p.ModifiedDate)pc_revised,p.ListPrice
+from Production.Product p,Production.ProductCostHistory pch
+where p.ProductID=pch.ProductID
+group by p.Name,p.ListPrice
+
+-- O.  check the department having more salary revision
+
+select * from HumanResources.Department  --deptid,Gname
+select * from HumanResources.EmployeePayHistory -- beid,rate
+select * from HumanResources.EmployeeDepartmentHistory --beid,dptid,
+
+select top 1 d.GroupName,count(eph.RateChangeDate) as revised from HumanResources.EmployeeDepartmentHistory edh
+join HumanResources.Department d on d.DepartmentID=edh.DepartmentID
+join HumanResources.EmployeePayHistory eph on eph.BusinessEntityID=edh.BusinessEntityID
+group by d.GroupName 
+order by revised desc
